e322ab8e1336e67c7a53b272fb148c160857358b
Molly McCarron <mccarronmollye@gmail.com>
2019-03-03 17:36:13 -0500

    Added Omniauth for Facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,6 +256,8 @@ Devise.setup do |config|
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
 
+  config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"]
+
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
