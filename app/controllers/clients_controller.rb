ebee535aebcdafe70dbcc8d0340d0d2a45d08217
Molly McCarron <mccarronmollye@gmail.com>
2019-03-04 16:11:34 -0500

    Client CRUD functioning swimmingly

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,43 @@
 class ClientsController < ApplicationController
+  def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(client_params)
+    #binding.pry
+    if @client.save
+      redirect_to @client
+    else
+      render :new
+    end
+    end
+
+  def show
+    @client = Client.find(params[:id])
+  end
+
+
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  def update
+    @client = Client.find(params[:id])
+
+    @client.update(yoga_class_params)
+
+    if @client.save
+     redirect_to @client
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def client_params
+    params.require(:client).permit(:org_name, :point_of_contact, :email)
+  end
 end
