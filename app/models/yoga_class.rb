ecce6ab5c5a5ed1e2a07871a476e12e01f788342
Molly McCarron <mccarronmollye@gmail.com>
2019-03-16 15:26:11 -0400

    removed bud that was preventing clients from being saved.

--- app/models/yoga_class.rb
+++ app/models/yoga_class.rb
@@ -1,6 +1,6 @@
 class YogaClass < ApplicationRecord
-  belongs_to :user, optional: true
-  belongs_to :client, optional: true#, inverse_of: :yoga_classes
+  belongs_to :user#, optional: true
+  belongs_to :client #, optional: true#, inverse_of: :yoga_classes
   validates :title, presence: true
   validates :class_plan, presence: true
 
