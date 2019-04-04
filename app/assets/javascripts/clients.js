706ccf52ebd3d95e30fd4ac8b142b0427fcf22be
Molly McCarron <mccarronmollye@gmail.com>
2019-04-04 15:37:47 -0400

    centered application

--- app/assets/javascripts/clients.js
+++ app/assets/javascripts/clients.js
@@ -6,7 +6,7 @@ $(function(){
 
 
       json.forEach(function(client){
-        $(".listed_clients").append("<h3>" + client.org_name + "</h3><li>" + client.point_of_contact + "</li><li>" + client.email + "</li><br>")
+        $(".listed_clients").append("<h3>" + client.org_name + "</h3><p>" + client.point_of_contact + "</p><p>" + client.email + "</p><br>")
       })
     })
   })
